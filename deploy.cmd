0485e6a69dc5089dcfd318af7d35bfab2c454809
Brant Fallin <brant.fallin@gmail.com>
2016-11-27 18:01:12 -0700

    Script to build & deploy nuspec

---
+++ deploy.cmd
@@ -0,0 +1,29 @@
+@echo off
+@cd /d "%~dp0"
+
+echo info: Setup MSBuild environment
+call "%VS140COMNTOOLS%VsMSBuildCmd.bat"
+
+echo info: Build Itc4net
+msbuild.exe ".\src\Itc4net.sln" /p:Configuration=Release /t:Rebuild
+if ERRORLEVEL 1 (
+    echo error: Failed to build Itc4net
+    goto EXIT
+)
+
+echo info: Pack NuGet nuspec
+for /f %%a IN ('dir /b *.nuspec') do (
+	nuget pack "%%a"
+)
+
+echo info: Push NuGet nupkg
+set /p APIKEY="Enter NuGet ApiKey:"
+for /f %%b IN ('dir /b *.nupkg') do (
+	rem nuget push "%%b" -ApiKey %APIKEY%
+)
+
+mkdir .\build
+move /y *.nupkg .\build
+
+:EXIT
+pause
