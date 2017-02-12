6c4aa99a2986b684b242c42d526e6c25a7ffe382
Brant Fallin <brant.fallin@gmail.com>
2017-02-11 18:42:32 -0700

    Improve OrderingOfEventsExample
    1. Make Message immutable (more realistic)
    2. Fix bug where p & q receive wrong msg
    3. Demonstrate partial ordering using Array.Sort

--- src/Itc4net.Tests/ScenarioTests.cs
+++ src/Itc4net.Tests/ScenarioTests.cs
@@ -80,29 +80,48 @@ namespace Itc4net.Tests
             Process p = new Process("p", fork.Item1);
             Process q = new Process("q", fork.Item2);
 
-            Message msgp1 = new Message("p1");
-            Stamp p1 = p.Send(msgp1);
+            Message msgp1 = p.Send("p1");
+            Stamp p1 = p.CurrentStamp;
             Console.WriteLine($"p1:{p1} after send");
 
-            Message msgq1 = new Message("q1");
-            Stamp q1 = q.Send(msgq1);
+            Message msgq1 = q.Send("q1");
+            Stamp q1 = q.CurrentStamp;
             Console.WriteLine($"q1:{q1} after send");
 
-            Stamp p2 = p.Receive(msgp1);
+            Stamp p2 = p.Receive(msgq1);
             Console.WriteLine($"p2:{p2} after receive");
 
-            Stamp q2 = q.Receive(msgq1);
+            Stamp q2 = q.Receive(msgp1);
             Console.WriteLine($"q1:{q1} after receive");
 
-            Stamp p3 = p.SomethingWithCausalSignificance();
+            Stamp p3 = p.Increment();
             Console.WriteLine($"p3:{p3} after event");
 
             p1.Leq(p2).Should().BeTrue();
             p1.Leq(p3).Should().BeTrue();
             p2.Leq(p3).Should().BeTrue();
 
+            q1.Leq(q2).Should().BeTrue();
+
+            p1.Leq(q2).Should().BeTrue(); // send occurs before receive
+            q1.Leq(p2).Should().BeTrue(); // send occurs before receive
+
             p1.Concurrent(q1).Should().BeTrue();
+            p2.Concurrent(q2).Should().BeTrue();
             p3.Concurrent(q2).Should().BeTrue();
+
+            // Stamp implements IComparable<Stamp> so a collection of stamps
+            // can be partially ordered using Sort methods. Note this is a
+            // partial order (not total order) as CompareTo returns 0
+            // for equal and concurrent stamps. 
+
+            var arr = new[] {q2, q1, p3, p2, p1};
+            Array.Sort(arr);
+
+            arr.Should().ContainInOrder(p1, p2, p3);
+            arr.Should().ContainInOrder(q1, q2);
+            arr.Should().ContainInOrder(p1, q2);
+            arr.Should().ContainInOrder(q1, p2, p3);
         }
 
         [Test(Description = "Example from Detection of Mutual Inconsistency in Distributed Systems, Fig. 1")]
@@ -258,12 +277,13 @@ namespace Itc4net.Tests
 
     class Message
     {
-        public string Payload { get; }
-        public Stamp Stamp { get; set; }
+        public string Text { get; }
+        public Stamp Stamp { get; }
 
-        public Message(string payload)
+        public Message(string text, Stamp stamp)
         {
-            Payload = payload;
+            Text = text;
+            Stamp = stamp;
         }
     }
 
@@ -278,29 +298,32 @@ namespace Itc4net.Tests
             _s = s;
         }
 
-        public Stamp Send(Message msg)
+        public Stamp CurrentStamp => _s.Peek();
+
+        public Message Send(string text)
         {
             Stamp anonymous;
             _s = _s.Send(out anonymous);
-            msg.Stamp = anonymous;
+
+            Message msg = new Message(text, anonymous);
 
             // e.g., _bus.Send(message);
-            Console.WriteLine($"{_name} snd '{msg.Payload}' with stamp:{msg.Stamp}");
-            return _s;
+            Console.WriteLine($"{_name} snd '{msg.Text}' with stamp:{msg.Stamp}");
+            return msg;
         }
 
         public Stamp Receive(Message msg)
         {
             // e.g., _bus.Receive(message);
             _s = _s.Receive(msg.Stamp);
-            Console.WriteLine($"{_name} rcv '{msg.Payload}' with stamp:{msg.Stamp}");
-            return _s;
+            Console.WriteLine($"{_name} rcv '{msg.Text}' with stamp:{msg.Stamp}");
+            return CurrentStamp;
         }
 
-        public Stamp SomethingWithCausalSignificance()
+        public Stamp Increment()
         {
             _s = _s.Event();
-            return _s;
+            return CurrentStamp;
         }
     }
 }
